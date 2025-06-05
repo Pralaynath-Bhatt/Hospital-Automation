9a0621997f2484c6db11950e2eda6549764cba13
Pralaynath-Bhatt <uniquecuber04@gmail.com>
2025-06-06 00:22:27 +0530

    test

---
+++ hospital/src/main/java/com/CAPS/hospital/Controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.CAPS.hospital.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String HomePage(){
+        return "Welcome Home";
+    }
+
+    @GetMapping("/hii")
+    public String ErrorPage(){
+        return "Welcome Error";
+    }
+}
