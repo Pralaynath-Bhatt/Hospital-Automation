e963cc441440823dc5c22d4fafa6de15bf17a607
Pralaynath-Bhatt <uniquecuber04@gmail.com>
2025-06-15 23:46:27 +0530

    Entity builting

--- hospital/src/main/java/com/CAPS/hospital/controller/HomeController.java
+++ hospital/src/main/java/com/CAPS/hospital/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.CAPS.hospital.Controller;
+package com.CAPS.hospital.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ hospital/src/main/java/com/CAPS/hospital/entity/Student.java
@@ -0,0 +1,18 @@
+package com.CAPS.hospital.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int  id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+}
