3f3948d834714b2dabca64700cdc0be8dd2b4ddd
Pralaynath-Bhatt <uniquecuber04@gmail.com>
2025-06-15 18:06:33 +0530

    database connected

--- hospital/src/main/java/com/CAPS/hospital/HospitalApplication.java
+++ hospital/src/main/java/com/CAPS/hospital/HospitalApplication.java
@@ -1,7 +1,9 @@
 package com.CAPS.hospital;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HospitalApplication {
@@ -10,4 +12,10 @@ public class HospitalApplication {
 		SpringApplication.run(HospitalApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner ->{
+		System.out.println("hello world");
+		};
+	}
 }
