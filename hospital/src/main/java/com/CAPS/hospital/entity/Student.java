7cd98a17de0575801d4b006591d59ab7f5510835
Pralaynath-Bhatt <uniquecuber04@gmail.com>
2025-06-16 02:16:16 +0530

    lombok implimentation

--- hospital/src/main/java/com/CAPS/hospital/entity/Student.java
+++ hospital/src/main/java/com/CAPS/hospital/entity/Student.java
@@ -1,9 +1,15 @@
 package com.CAPS.hospital.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "student")
+@Builder
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,4 +21,14 @@ public class Student {
     private String lastName;
     @Column(name = "email")
     private String email;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
