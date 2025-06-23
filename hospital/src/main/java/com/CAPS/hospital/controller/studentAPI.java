ef68857276750db54bd7ae5d230ada5da87e5c76
Pralaynath-Bhatt <uniquecuber04@gmail.com>
2025-06-23 20:25:40 +0530

    melody itni chocolaty kyu hai

--- hospital/src/main/java/com/CAPS/hospital/controller/studentAPI.java
+++ hospital/src/main/java/com/CAPS/hospital/controller/studentAPI.java
@@ -1,7 +1,11 @@
 package com.CAPS.hospital.controller;
 
 import com.CAPS.hospital.entity.Student;
+import com.CAPS.hospital.studentDAO.StudentDAO;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +15,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class studentAPI {
-    @GetMapping("/student")
-    public List<Student> getStudentsAPI(){
-        List<Student> students = new ArrayList<>();
-        students.add(new Student("Om","Puri","om@gmail.com"));
-        students.add(new Student("Ram","Puri","om@gmail.com"));
-        students.add(new Student("Naresh","Puri","om@gmail.com"));
-        students.add(new Student("Shreya","Puri","om@gmail.com"));
-        return students;
+    StudentDAO studentDAO;
+    @Autowired
+    public studentAPI(StudentDAO studentDAO) {
+        this.studentDAO = studentDAO;
+    }
+
+    @GetMapping("/student/{studentid}")
+    public Student getstudent(@PathVariable int studentid){
+
+        Student student=studentDAO.findById(studentid);
+        return student;
     }
 }
